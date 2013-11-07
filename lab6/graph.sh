2f7336b604b02e244b90ba6593f7c8602812dbbc
Will Jackson <greenerside71@gmail.com>
2013-11-07 15:09:46 -0500

    Made shell script nicer

--- lab6/graph.sh
+++ lab6/graph.sh
@@ -5,18 +5,21 @@ FILE2='./p4_output.csv'
 PRECS='0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0.00005 0.00001 0.000005 0.000001 0.0000005 0.0000001'
 GNUPLOT="set datafile sep ','; set logscale x; plot '$FILE1' u 1:4 w lines, '$FILE2' u 1:4 w lines"
 
-# Clear out files if they exist
-rm -f $FILE1 $FILE2
+# If files don't exist, create them
+if ! ([ -f $FILE1 ] && [ -f $FILE2 ])
+then
+    echo "Not Exist!"
+    exit
+    # Call p3, p4 with increasing values for precision
+    for i in $PRECS
+    do
+        echo "Running at precision $i..."
+        mpirun -np 200 ./p3 3.0 5.0 $i
+        mpirun -np 200 ./p4 3.0 5.0 $i
+        echo
 
-# Call p3, p4 with increasing values for precision
-for i in $PRECS
-do
-    echo "Running at precision $i..."
-    mpirun -np 200 ./p3 3.0 5.0 $i
-    mpirun -np 200 ./p4 3.0 5.0 $i
-    echo
-
-done
+    done
+fi
 
 # Graph em!
 gnuplot -persist -e "$GNUPLOT"
